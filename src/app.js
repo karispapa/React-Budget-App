9b3604ecb0d432fdb30a66082f835f8360cef249
karispapa <skariuki91@gmail.com>
2020-05-11 07:47:03 +0300

    Removed comments and unnecessary imports

--- src/app.js
+++ src/app.js
@@ -4,31 +4,14 @@ import AppRouter from './routes /AppRouter';
 import './styles/style.css';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
-// import storeSynchronize from 'redux-localstore';  
-import { saveState} from './localStorage'
-import arraySplit from './js/arraySplit';
-import {getTotalIncome, getTotalExpenses, calculateTotals} from './actions/totalsActions'
+import { saveState} from './localStorage';
 
 
 const store = configureStore();
 
-// store.subscribe(()=>{
-//   store.dispatch(getTotalIncome(arraySplit(store.getState().items).income))
-//   store.dispatch(getTotalExpenses(arraySplit(store.getState().items).expenses))
-// })
-
-
 store.subscribe(()=>{
   saveState(store.getState())
-  // console.log(store.getState())
-// console.log(store.getState().items)
-// console.log(arraySplit(store.getState().items))
-// console.log(calculateTotals(arraySplit(store.getState().items).income))
-// console.log(calculateTotals(arraySplit(store.getState().items).expenses))
 })
-
-
-
 
 const jsx = (
   <Provider store={store}>
